b2d115be474a261d83bd2f02fd461a5b09a55e9d
fpr <fabien.perroquin@wi6labs.com>
2017-12-11 17:00:33 +0100

    Add Low Power API
    Signed-off-by: fpr <fabien.perroquin@wi6labs.com>

---
+++ src/STM32LowPower.cpp
@@ -0,0 +1,119 @@
+/**
+******************************************************************************
+* @file    STM32LowPower.cpp
+* @author  WI6LABS
+* @version V1.0.0
+* @date    11-December-2017
+* @brief   Provides a STM32 Low Power interface with Arduino
+*
+******************************************************************************
+* @attention
+*
+* <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*   1. Redistributions of source code must retain the above copyright notice,
+*      this list of conditions and the following disclaimer.
+*   2. Redistributions in binary form must reproduce the above copyright notice,
+*      this list of conditions and the following disclaimer in the documentation
+*      and/or other materials provided with the distribution.
+*   3. Neither the name of STMicroelectronics nor the names of its contributors
+*      may be used to endorse or promote products derived from this software
+*      without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+******************************************************************************
+*/
+
+#include "STM32LowPower.h"
+
+STM32LowPower LowPower;
+
+STM32LowPower::STM32LowPower()
+{
+
+}
+
+void STM32LowPower::begin(void)
+{
+
+}
+
+void STM32LowPower::idle(void)
+{
+  idle(0);
+}
+
+void STM32LowPower::idle(uint32_t millis)
+{
+  if(millis > 0) {
+    //Enable RTC
+  }
+}
+
+void STM32LowPower::sleep(void)
+{
+  sleep(0);
+}
+
+void STM32LowPower::sleep(uint32_t millis)
+{
+  if(millis > 0) {
+    //Enable RTC
+  }
+}
+
+void STM32LowPower::deepSleep(void)
+{
+  deepSleep(0);
+}
+
+void STM32LowPower::deepSleep(uint32_t millis)
+{
+  if(millis > 0) {
+    //Enable RTC
+  }
+}
+
+void STM32LowPower::shutdown(void)
+{
+  shutdown(0);
+}
+
+void STM32LowPower::shutdown(uint32_t millis)
+{
+  if(millis > 0) {
+    //Enable RTC
+  }
+}
+
+void STM32LowPower::attachInterruptWakeup(uint32_t pin, voidFuncPtr callback, uint32_t mode)
+{
+  //How enable the WakeUp pins???? Should we
+}
+
+void STM32LowPower::enableWakeupFrom(HardwareSerial *serial, voidFuncPtr callback)
+{
+
+}
+
+void STM32LowPower::enableWakeupFrom(TwoWire *wire, voidFuncPtr callback)
+{
+
+}
+
+void STM32LowPower::enableWakeupFrom(STM32RTC *rtc, voidFuncPtr callback)
+{
+  //Is this function is really usefull? I not sure...
+}
